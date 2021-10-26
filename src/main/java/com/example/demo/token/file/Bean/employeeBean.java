1b36207e0668888e1edaa24ea1154867e097be33
smvidyasree <you@example.com>
2021-10-26 10:38:44 +0530

    changes done

--- src/main/java/com/example/demo/token/file/Bean/employeeBean.java
+++ src/main/java/com/example/demo/token/file/Bean/employeeBean.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonInclude(value = Include.NON_NULL)
 public class employeeBean {
 //	private int id;
-	private String username;
+	private String username
 	private String fullname;
 	private String password;
 	private String emailid;
